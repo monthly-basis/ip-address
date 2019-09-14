ee17a724ecb8058c2da425d98fa0cae514319805
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-09-14 19:03:32 +0000

    `created` timestamp should never be null

--- sql/test/banned_first_three_quadrants/create.sql
+++ sql/test/banned_first_three_quadrants/create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `banned_first_three_quadrants` (
     `first_three_quadrants` varchar(11) NOT NULL,
-    `created` datetime DEFAULT NULL,
+    `created` datetime NOT NULL,
     UNIQUE KEY `first_three_quadrants` (`first_three_quadrants`)
 ) CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
