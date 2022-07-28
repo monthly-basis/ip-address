77a21ded6f1a4852e48f21fa670e392d694dd0d5
Leo Galleguillos <leo.galleguillos@icloud.com>
2022-07-28 21:32:09 +0000

    Increase column width

--- sql/test/banned_first_four_segments/create.sql
+++ sql/test/banned_first_four_segments/create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `banned_first_four_segments` (
-    `first_four_segments` varchar(15) NOT NULL,
+    `first_four_segments` varchar(19) NOT NULL,
     `created` datetime NOT NULL,
     UNIQUE KEY `first_four_segments` (`first_four_segments`)
 ) CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
